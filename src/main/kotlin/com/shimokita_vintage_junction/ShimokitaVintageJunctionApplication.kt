9a891642d4f718355e5c4d39d6729bf8be33d206
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2024-12-01 23:05:30 +0900

    first commit

---
+++ src/main/kotlin/com/shimokita_vintage_junction/ShimokitaVintageJunctionApplication.kt
@@ -0,0 +1,11 @@
+package com.shimokita_vintage_junction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShimokitaVintageJunctionApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShimokitaVintageJunctionApplication>(*args)
+}
